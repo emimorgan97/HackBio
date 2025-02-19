e951e63aea9632805f3800d00d4e1ca0d9925e90
emimorgan97 <ealmorgan@gmail.com>
2025-02-19 17:20:25 -0500

    update

--- stage_1/stage_1.py
+++ stage_1/stage_1.py
@@ -33,10 +33,54 @@ def translate_DNA_Protein(sequence: str):
     while (stop  == False):
         if sequence[i:i+3] in codon_table:
             protein.append(codon_table[sequence[i:i+3]])
-            i += 3
-        if codon_table[sequence[i:i+3]] == "Stop":
+            if codon_table[sequence[i:i+3]] == "Stop":
+                stop = True
+        elif i> len(sequence):
             stop = True
+        i += 3
     return protein
 
 mrna = "AUGGCCAUGGCGCCCAGAACUGAGAUCAAUAGAUGG"
 print(translate_DNA_Protein(mrna))
+
+from numpy import random
+import numpy as np
+import pandas as pd
+
+x = 100
+
+y = random.randint(0,x)
+z = random.randint(0,x)
+
+lag = min(y,z)
+exp = max(y,z)
+
+
+def logistic_growth(starting_pop: int, carrying_cap: int, growth_rate: float):
+
+    x = 100
+    y = random.randint(0,x)
+    z = random.randint(0,x)
+    lag = min(y,z)
+    exp = max(y,z)
+    # Generate time points
+    t = np.linspace(0, exp, 100)
+
+    # Compute population values
+    P = np.piecewise(t, [t < lag, t >= lag],
+                    [starting_pop, lambda t: starting_pop * np.exp(growth_rate * (t - lag))])
+
+    # Convert to a list
+    population_list = P.tolist()
+    return population_list
+
+
+
+def data_frame(starting_pop: int, carrying_cap: int, growth_rate: float):
+    df = pd.DataFrame()
+    for i in range(100):
+        population = logistic_growth(starting_pop,  carrying_cap, growth_rate)
+        df[f"Column_{i}"] = population
+    return df
+
+print(data_frame(100, 1000, 0.1))
